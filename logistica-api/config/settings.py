7f4bf9897a62066fa95361a85974c4c034426653
HLDataDev <hllagas@gmail.com>
2026-06-16 20:02:50 -0500

    actualizacion del proyecto al 16-jun

--- logistica-api/config/settings.py
+++ logistica-api/config/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Configuración de drf-spectacular para la documentación OpenAPI
 SPECTACULAR_SETTINGS = {
