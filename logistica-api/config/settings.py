c2960d9fab7c560c6cf2b2230b9c6872f550d1cd
HLDataDev <hllagas@gmail.com>
2026-06-16 21:48:59 -0500

    implementacion del proyecto

--- logistica-api/config/settings.py
+++ logistica-api/config/settings.py
@@ -40,7 +40,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://logistica-frontend-git-main-henry-llagas-projects.vercel.app/',
-    'https://tu-frontend.com,http://localhost:5173',
+    'https://tu-frontend.com',
+    'http://localhost:5173',
 ]
 
 
